99807f82ccf6e4662365fc83df96d13a262702b9
Ezekial Pack <103347950+APointProven@users.noreply.github.com>
2023-11-09 03:32:51 -0500

    Added Time To Text

---
+++ src/TimeToText.lua
@@ -0,0 +1,17 @@
+-- Converts seconds to time format
+local time = {}
+
+function time:Convert(s, bool) -- s(seconds), bool(whether you want hours or not)
+    local mins = string.format("%02i:%02i", s/60%60, s%60)
+    local hrs = string.format("%02i:%02i:%02i", s/60^2, s/60%60, s%60)
+    if bool == nil then
+        if (s/60^2) < 1 then
+            return mins
+        else
+            return hrs
+        end
+    end
+    if bool then return hrs end
+end
+
+return time
